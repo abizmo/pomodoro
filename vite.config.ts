3f4f5e3455bc1af6b431a4d6c7d0405e0c7880e3
Abián Izquierdo <abizmo@gmail.com>
2023-04-05 15:33:45 +0100

    build: prepare to use svgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
